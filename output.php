f23f14bd999cabe5c5c464a5d4e072daedaa6115
Ďurovič Róbert <durovicr@brno.local>
2020-05-20 14:19:29 +0200

    change output format

--- output.php
+++ output.php
@@ -3,54 +3,32 @@ include_once 'config.php';
 
 ?>
 
-
-<!DOCTYPE html>
-<html lang="cz">
-<head>
-    <meta http-equiv="Content-type" content="text/html; charset=UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- CSS STYLE -->
-    <link rel="stylesheet" href="<?php echo $basePath . "/css/bootstrap.min.css"?>"> 
-   
-    <title>Export CSV</title>
-</head>
-<body>
-    <div class="mt-2">
-        <div class="ml-2">
-            <div class="col-md-12">
-                <table class="">
-                    <thead class="strong">
-                        <tr>
-                            <th class="px-2">Datum (Date)</th>
-                            <th class="px-2">Částka (Amount)</th>
-                            <th class="px-2">Popis platby (Comment)</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    <?php
-                    // GET DATA
-                    $result = $db->query("SELECT * FROM CSV_IMPORT ORDER BY id ASC");
-                    $cnt = 0;                    
-                        while($row = $result->fetch_assoc()){
-                            $numberAsString = number_format($row['amount'], 2, ",", " ");
-                    ?>
-                        <tr>
-                            <td class="px-2"><?php echo $row['payment_date']; ?></td>
-                            <td class="px-2"><?php echo $numberAsString . " Kč"; ?></td>
-                            <td class="px-2"><?php echo $row['comment']; ?></td>
-                        </tr>
-                    <?php $cnt++; } ?>
-                    </tbody>
-                </table>
-                <?php 
-                    // IF THERE ARE NO DATA TO RENDER
-                    if ($cnt == 0) {
-                        echo "<p class='" . "alert alert-warning text-center" . "'>Žádné data!</p>";
-                    }
-                ?>
-            </div>
-        </div>
-    </div>
+<table id="tablepress-1" class="tablepress tablepress-id-1">
+    <thead>
+        <tr class="row-1 odd">
+            <th class="column-1">Datum (Date)</th><th class="column-2">Částka (Amount)</th><th class="column-3">Popis platby (Comment)</th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php
+    // GET DATA
+    $result = $db->query("SELECT * FROM CSV_IMPORT ORDER BY id ASC");
+    $cnt = 0;
+    $numRow = 2;               
+        while($row = $result->fetch_assoc()){
+            $numberAsString = number_format($row['amount'], 2, ",", " ");
+    ?>
+    <tr <?php echo ('class="row-' . $numRow++); if ($numRow % 2 == 0) { echo " odd" . '"'; } else {echo " even" . '"';} ?>>
+            <td class="column-1"><?php echo $row['payment_date']; ?></td><td class="column-2"><?php echo $numberAsString . " Kč"; ?></td><td class="column-3"><?php echo $row['comment']; ?></td>
+        </tr>
+    <?php $cnt++; } ?>
+    </tbody>
+</table>
+<?php 
+    // IF THERE ARE NO DATA TO RENDER
+    if ($cnt == 0) {
+        echo "<p>Žádné data!</p>";
+    }
+?>
 </body>
 </html>
